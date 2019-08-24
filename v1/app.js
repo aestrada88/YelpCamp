26895d6bbd60b78e9f63b9e70ffb951e5761651b
aestrada88 <aestrada88@gmail.com>
2019-08-23 23:43:33 -0400

    functionality, needs styling

---
+++ v1/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const campgrounds = [
+  { name: "Wekiva", image: "https://media.istockphoto.com/photos/camping-tent-in-a-camping-in-a-forest-by-the-river-picture-id911995140?k=6&m=911995140&s=612x612&w=0&h=U-yG-2eR8pOxLX_G8Eg9fDI1SOWYifxbb4BiiOhNNiI=" },
+  { name: "Koa", image: "https://www.reserveamerica.com/owp-webclient/assets/images/ra/articles_camping_8.jpg" },
+  { name: "Salmon Creek", image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/camping-quotes-1556677391.jpg?crop=0.588xw:1.00xh;0.157xw,0&resize=640:*" }
+];
+
+app.get("/", (req, res) => {
+  res.render("landing")
+});
+
+app.get("/campgrounds", (req, res) => {
+  res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/campgrounds", (req, res) => {
+  const name = req.body.name;
+  const image = req.body.image;
+  const newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Yelp Camp Server Has Started!");
+});
